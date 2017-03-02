2e676ad0296832e74c371f5d1729c9a364c3216a
PirateKingDee <andyliang@Andys-MacBook-Pro-3.local>
2017-03-01 21:08:38 -0800

    Start PrimsMST

---
+++ cpp/cs331/graphs/PrimsMST.java
@@ -0,0 +1,21 @@
+package edu.cpp.cs331.graphs;
+
+public class PrimsMST extends MinimumSpanningTree{
+  public Graph genMST(Graph g){
+    //set up Neighbor
+    //put v0 in graph
+    //store all neighbor edges in an ArrayList
+    //heapify the list
+    //while the list is not empty
+      //get the least weight edge(first one)
+        //if(e.v1 and e.v2 are in the graph)
+          //pop the list and continue
+        //else
+          //graph add both vertex
+          //graph add edge
+          //get neighbors of e.v2
+          //add all its neighbor to list
+          //pop the list
+    return null;
+  }
+}
